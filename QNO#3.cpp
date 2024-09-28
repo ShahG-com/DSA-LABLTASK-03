262df035fc3df6b17d038313c9fd4854c9e372b6
ShahG-com <shahzaibadil03@gmail.com>
2024-09-28 13:53:07 +0500

    Part#3

---
+++ QNO#3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int array[] = {22, 34, 25, 32, 55};
+    array[4]=100;
+    for(int i=0; i<5; i++)
+    {
+        cout<<array[i]<<"  ";
+    }
+
+
+return 0;
+}
